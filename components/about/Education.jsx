d1f03a6b16bcca1ef0f597a5bab83820710e41dd
cgranda <Cesar.Granda@baruch.cuny.edu>
2023-06-28 16:48:39 -0400

    added education from mondb

--- components/about/Education.jsx
+++ components/about/Education.jsx
@@ -1,9 +1,31 @@
-import educationContent from "../../data/education";
+// import educationContent from "../../data/education";
+import { useState, useEffect } from 'react';
 
 const Education = () => {
+  function EducationDb() {
+      const [edu, setEdu] = useState([]);
+      useEffect(()=> {
+        eduMDB()
+      }, [])
+
+      const eduMDB = async () => {
+          const response = await fetch('https://resume-mern.onrender.com/fetch-edu');
+
+          setEdu(await response.json())
+      }
+  function compare( a, b ) {
+    if ( a.year < b.year ){
+      return 1;
+    }
+    if ( a.year > b.year ){
+      return -1;
+    }
+    return 0;
+  }
+  edu.sort(compare)  
   return (
     <ul>
-      {educationContent.map((val, i) => (
+      {edu.map((val, i) => (
         <li key={i}>
           <div className="icon">
             <img src="img/about/book.png" alt="icon" />
@@ -11,9 +33,9 @@ const Education = () => {
           <span className="time open-sans-font text-uppercase">{val.year}</span>
           <h5 className="poppins-font text-uppercase">
             {val.degree}
-            <span className="place open-sans-font">{val.institute}</span>
+            <span className="place open-sans-font">{val.institution}</span>
           </h5>
-          <p className="open-sans-font">{val.details}</p>
+          <p className="open-sans-font">{val.field}</p>
         </li>
       ))}
     </ul>
